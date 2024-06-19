6dd82095c6a35e4c037f81832d453144714c3bee
Kegan Maher <kegan@compiler.la>
2024-06-19 03:57:56 +0000

    refactor(notebooks): hubspot response to DataFrame

--- data/utils.py
+++ data/utils.py
@@ -1,4 +1,10 @@
+from typing import Any
 import pandas as pd
+
+
+def hubspot_to_df(response: Any) -> pd.DataFrame:
+    """Helper converts a Hubspot response with `{ 'results': [ {...}, {...}, ...] }` into a DataFrame of records."""
+    return pd.json_normalize(response.to_dict(), "results")
 
 
 def write_json_records(df: pd.DataFrame, file_path: str):
