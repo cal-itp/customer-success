1571203784112b0009fd75c1a5df62b7a7588531
Kegan Maher <kegan@compiler.la>
2024-06-20 19:11:45 +0000

    refactor(utils): concat one or more hubspot responses

--- data/utils.py
+++ data/utils.py
@@ -5,8 +5,35 @@ import pandas as pd
 
 
 def hubspot_to_df(response: Any) -> pd.DataFrame:
-    """Helper converts a Hubspot response with `{ 'results': [ {...}, {...}, ...] }` into a DataFrame of records."""
-    return pd.json_normalize(response.to_dict(), "results")
+    """
+    Converts a Hubspot response like:
+
+        ```
+        { "results": [ {...}, {...}, ...] }
+        ```
+
+    or list of responses like:
+
+        ```
+        [
+            { "results": [ {...}, {...}, ...] },
+            { "results": [ {...}, {...}, ...] },
+            ...
+        ]
+        ```
+
+    into a single DataFrame of records.
+    """
+    # assume a list input
+    if not isinstance(response, list):
+        response = [response]
+
+    # create a list of DataFrames by normalizing results from each response
+    dfs = [pd.json_normalize(r.to_dict(), "results") for r in response]
+
+    # concatenate all the DataFrames together into one
+    # ignore index since it doesn't carry any information (essentially, row number)
+    return pd.concat(dfs, ignore_index=True)
 
 
 def write_json_records(df: pd.DataFrame, file_path: str):
