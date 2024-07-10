e13a1283786da75e73d41b0dfaeb5471ba19b9b0
Kegan Maher <kegan@compiler.la>
2024-07-10 02:01:44 +0000

    feat(notes/post): generate a Slack message for each note

--- notes/post.py
+++ notes/post.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 import json
+from typing import Generator
 
 from bs4 import BeautifulSoup
+from slack_sdk.models.blocks import HeaderBlock, SectionBlock, MarkdownTextObject
 
 from notes.download import NOTES_PATH
 
@@ -37,3 +39,27 @@ def process_notes(notes: list[Note]) -> list[Note]:
         note.created_at = int(int(note.created_at) / 1000)
 
     return notes
+
+
+def create_messages(notes: list[Note]) -> Generator[dict, None, None]:
+    # see https://api.slack.com/reference/surfaces/formatting
+
+    for note in notes:
+        target_name = note.name_company or note.name_vendor
+        target_type = "Transit Agency" if note.name_company else "Vendor"
+        header = f"[{target_type}] {target_name}:"
+
+        note_text = f"> {note.body}"
+        created_by_text = f"*Created by:*\n{note.name_user}"
+        date_fmt = f"<!date^{note.created_at}^{{date_long}} at {{time}}|{note.created_at}>"
+        created_on_text = f"*Created on:*\n{date_fmt}"
+
+        yield dict(
+            channel=CHANNEL_ID,
+            text=header,
+            blocks=[
+                HeaderBlock(text=header),
+                SectionBlock(text=MarkdownTextObject(text=note_text)),
+                SectionBlock(fields=[MarkdownTextObject(text=created_by_text), MarkdownTextObject(text=created_on_text)])
+            ],
+        )
