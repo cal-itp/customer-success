91d3af40addf400492d8243e0954c0ddbe1416e9
Scott Cranfill <scranfill@compiler.la>
2026-05-19 21:37:25 +0000

    feat: Add ability to export full email body content to JSON files
    Updates data.utils.hubspot_get_all_pages to be able to directly output to CSV or JSON, and to do that in chunks when it's expected that the total data will be too much for one file.

--- data/utils.py
+++ data/utils.py
@@ -20,13 +20,40 @@ def getattr_or_get(obj, attr):
         return obj.get(attr)
 
 
-def hubspot_get_all_pages(hubspot_api, page_size=10, max_pages=sys.maxsize, **kwargs) -> list:
+def hubspot_get_all_pages(
+    hubspot_api,
+    page_size=10,
+    max_pages=sys.maxsize,
+    output=False,
+    output_format="",
+    output_filename=None,
+    output_chunk_size=None,
+    **kwargs,
+) -> list:
     """Repeatedly call the `get_page` method of the given Hubspot API until all responses are received.
+
+    Results can be sent directly to a CSV or JSON file by setting `output` to True and `output_format` to "csv" or "json".
+    `output_filename` should not include the extension; that will be added based on `output_format`.
+    If no `output_filename` is given, it will default to the string value of `hubspot_api`.
+    If the total results are expected to be too large for a single file, they can be chunked into multiple files
+    by setting `output_chunk_size` to the number of pages you want to save into each file.
+    For example, if there are 20,000 records, you can get 20 files of 1,000 records each
+    by setting `page_size` to 100 (which is the max HubSpot allows) and `output_chunk_size` to 10.
 
     Extra kwargs are passed through to the `get_page` method.
     """
     kwargs["limit"] = page_size
     pages = []
+
+    if output:
+        # Set up some things for later use
+        filename = output_filename or str(hubspot_api)
+        match output_format:
+            case "csv":
+                output_method = write_csv_records
+            case "json":
+                output_method = write_json_records
+        counter = 1
 
     response = hubspot_api.get_page(**kwargs)
     pages.append(response)
@@ -35,6 +62,22 @@ def hubspot_get_all_pages(hubspot_api, page_size=10, max_pages=sys.maxsize, **kw
         kwargs["after"] = getattr_or_get(getattr_or_get(getattr_or_get(response, "paging"), "next"), "after")
         response = hubspot_api.get_page(**kwargs)
         pages.append(response)
+
+        if output and output_chunk_size and (len(pages) == output_chunk_size or getattr_or_get(response, "paging") is None):
+            # If we're outputting in chunks
+            #   - and we've landed on the page number matching output_chunk size
+            #   - or we're on the last page (the current response's "paging" value is None)
+
+            # Output this chunk with the current counter appended to the filename
+            output_method(hubspot_to_df(pages), f"{filename}_{counter}.{output_format}")
+
+            # Reset pages and increment the counter for the next chunk
+            pages = []
+            counter += 1
+
+    if output and not output_chunk_size:
+        # If we're outputting but not in chunks, output the complete pages list
+        output_method(hubspot_to_df(pages), f"{filename}.{output_format}")
 
     return pages
 
